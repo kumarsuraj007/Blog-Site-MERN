00decf7e44c102ba5c0ca4892bd2ee6247f816fe
Suraj Kumar <109417154+kumarsuraj007@users.noreply.github.com>
2023-08-07 09:03:23 +0530

    removed some minor changes

--- client/src/routes/navigation/navigation.component.jsx
+++ client/src/routes/navigation/navigation.component.jsx
@@ -1,30 +1,43 @@
-import { Outlet, Link } from 'react-router-dom'
-import './navigation.styles.css'
+import { useContext } from "react";
+import { Outlet, Link } from "react-router-dom";
+import { UserContext } from "../../contexts/userContext.jsx";
+import "./navigation.styles.css";
 
-function Navigation () {
+function Navigation() {
+  const { setCurrentUser } = useContext(UserContext);
   const logoutUser = () => {
     localStorage.clear();
-  }
- 
+    setCurrentUser(null);
+  };
+
   return (
     <>
-    <div className='nav'>
-    <Link to='/'><h1>BLOG PIX</h1></Link>
+      <div className="nav">
+        <Link to="/">
+          <h1>BLOG PIX</h1>
+        </Link>
         <ul>
-        <li><a href='/createpost'>CREATE POST</a></li>
-        {
-              localStorage.getItem('jwt') !== null ? (
-                <Link to='/login'><li onClick={logoutUser}>LOGOUT</li></Link>
-              ) : (
-                <Link to='/login'><li>LOGIN</li></Link>
-              )
-            }
-            
+          {localStorage.getItem("jwt") !== null ? (
+            <Link to="/createpost">
+              <li>CREATE POST</li>
+            </Link>
+          ) : (
+            <li></li>
+          )}
+          {localStorage.getItem("jwt") !== null ? (
+            <Link to="/login">
+              <li onClick={logoutUser}>LOGOUT</li>
+            </Link>
+          ) : (
+            <Link to="/login">
+              <li>LOGIN</li>
+            </Link>
+          )}
         </ul>
-    </div>
-    <Outlet />
+      </div>
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default Navigation 
+export default Navigation;
